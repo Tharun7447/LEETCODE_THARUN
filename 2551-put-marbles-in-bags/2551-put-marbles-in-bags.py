71303dabb1e70ad2589663f4cb56626963c4962e
Tharun7447 <149917599+Tharun7447@users.noreply.github.com>
2025-03-31 15:05:16 +0530

    Time: 122 ms (89.67%), Space: 30.3 MB (62.81%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        if k == 1:
+            return 0
+
+        pair_sums = [weights[i] + weights[i + 1] for i in range(len(weights) - 1)]
+        pair_sums.sort()
+
+        min_score = sum(pair_sums[:k-1])
+        max_score = sum(pair_sums[-(k-1):])
+
+        return max_score - min_score
